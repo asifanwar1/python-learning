3a6fcf979960dd1d55a2a7428deddc52ca2ead6c
asifanwar1 <asifanwar0705@gmail.com>
2026-04-29 03:26:10 +0500

    lesson 9

---
+++ lesson9.py
@@ -0,0 +1,11 @@
+# Build a Number Pattern Generator
+
+def number_pattern(n):
+    if not isinstance(n, int):
+        return "Argument must be an integer value."
+    if n < 1:
+        return "Argument must be an integer greater than 0."
+    result = []
+    for num in range(1, n + 1):
+        result.append(str(num))
+    return ' '.join(result)
